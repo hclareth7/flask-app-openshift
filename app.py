2c1ffa3e3e0c00ad4554b8ca1407e9a75e7580f0
Hernando Ariza Perez <harizape@harizape-thinkpadp1gen5.rmtco.csb>
2024-09-01 20:55:05 -0500

    Adding  helloworld

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, red hat!'
+    return 'Hello, Hernando this is an update!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
